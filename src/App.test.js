3d30bc3dcab4d4488d5805279092b494678c238e
srikanthpoosa <poosalasreekanth52@gmail.com>
2026-05-09 16:09:11 +0530

    app change

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders app title', () => {
-  render(<App />);
-  const textElement = screen.getByText(/react/i);
-  expect(textElement).toBeInTheDocument();
-});
