ad0017f4bbc5166a0d8f6b5042ea589c4a576bfc
srikanthpoosa <poosalasreekanth52@gmail.com>
2026-05-11 12:05:12 +0530

    comment to skip [skip ci]

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,8 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+//this is to skip the ci
+
 test('renders app title', () => {
   render(<App />);
   const textElement = screen.getByText(/react/i);
